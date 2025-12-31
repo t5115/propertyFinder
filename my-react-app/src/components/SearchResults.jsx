3f897f8acd500ab40cd93922f01c05fef4f8f6c1
Tahmid Ahmed <tahmid.ahmed5115@gmail.com>
2025-12-31 16:21:04 +0000

    saving progress

--- my-react-app/src/components/SearchResults.jsx
+++ my-react-app/src/components/SearchResults.jsx
@@ -3,7 +3,7 @@
 function SearchResults({amount}){
     return(
         <div className="search-container">
-            <p>Searched returned {amount} propertie(s)</p>
+            <p>Searched returned {amount} propertie(s)</p> {/*Takes in number of properties found and displays at top*/}
         </div>
     );
 }
